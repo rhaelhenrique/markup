ddb7effb7992505a60bee5bf1bf9c546b136c254
Rhael Henrique <rhaelhenriquerodrigues@gmail.com>
2021-08-03 22:39:43 -0300

    Comit
    Comit dos arquivos:
    index.html
    styles.css
    scripts.js

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# markup
+## Markup
+##
+### Ferramenta de marcação com timer.
+
+Pode ser aplicado de diversas formas, mas a ideia base é para ser usado em produções audiovisuais.
+Você inicia o Cronômetro (botão Start) e faz as marcações utilizando o botão Markup para salvar aquele instante de tempo.
+Em seguida pode fazer as anotações necessárias para aquele ponto.
+
+O cronômetro também tem as opções de Pause e Reset.
+
+##### Compartilhe essa ferramenta!
+##### Deixe seu feedback: https://www.linkedin.com/in/rhaelhenrique
---
+++ scripts.js
@@ -0,0 +1,88 @@
+
+let hour = 0;
+let minute = 0;
+let second = 0;
+let millisecond = 0;
+
+let cron;
+
+//Clean field
+  document.getElementById("text").value = "";
+
+document.form_main.start.onclick = () => start();
+document.form_main.pause.onclick = () => pause();
+document.form_main.reset.onclick = () => reset();
+document.form_main.markup.onclick = () => markup();
+document.form_main.save.onclick = () => save();
+
+function start() {
+  pause();
+  cron = setInterval(() => { timer(); }, 10);
+}
+
+function pause() {
+  clearInterval(cron);
+}
+
+function reset() {
+  var result = confirm("Confirma o Reset?");
+  if (result == true){
+    hour = 0;
+    minute = 0;
+    second = 0;
+    millisecond = 0;
+    document.getElementById('hour').innerText = '00';
+    document.getElementById('minute').innerText = '00';
+    document.getElementById('second').innerText = '00';
+    document.getElementById('millisecond').innerText = '000';
+  } else {
+      exit;
+  }
+  
+}
+
+function timer() {
+  if ((millisecond += 10) == 1000) {
+    millisecond = 0;
+    second++;
+  }
+  if (second == 60) {
+    second = 0;
+    minute++;
+  }
+  if (minute == 60) {
+    minute = 0;
+    hour++;
+  }
+  document.getElementById('hour').innerText = returnData(hour);
+  document.getElementById('minute').innerText = returnData(minute);
+  document.getElementById('second').innerText = returnData(second);
+  document.getElementById('millisecond').innerText = returnData(millisecond);
+}
+
+function returnData(input) {
+  return input > 10 ? input : `0${input}`
+}
+
+function markup() {
+  document.getElementById('hour2').innerText = returnData(hour);
+  document.getElementById('minute2').innerText = returnData(minute);
+  document.getElementById('second2').innerText = returnData(second);
+  document.getElementById('millisecond2').innerText = returnData(millisecond);
+}
+
+function save() {
+  const auxMark = hour+":"+minute+":"+second+":"+millisecond;
+  console.log(auxMark);
+  var text = "";
+  text = document.getElementById("text").value;
+  const aux = auxMark+" ::: "+text;
+  console.log(aux);
+  
+  var auxh3 = document.createElement("H3");
+  auxh3.innerHTML = aux;
+  document.body.appendChild(auxh3);
+  
+  //Clean field
+  document.getElementById("text").value = "";
+}
